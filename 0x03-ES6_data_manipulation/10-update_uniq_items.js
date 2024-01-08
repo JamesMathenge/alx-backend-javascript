948548c6d1558dcd27acb37e539861dc803b5e02
James Mathenge <mathengejames64@gmail.com>
2024-01-08 22:03:25 +0300

    task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,10 +2,12 @@ export default function updateUniqueItems(map) {
     if (map instanceof Map) {
       for (const [key, value] of map) {
         if (value === 1) {
-          updatedMap.set(key, 100);
+          map.set(key, 100);
         }
       }
+  
       return map;
     }
+  
     throw new Error('Cannot process');
   }
