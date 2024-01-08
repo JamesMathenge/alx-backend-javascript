5341ebae1d199c5cb24c5e2a7cf6086d8c52ac93
James Mathenge <mathengejames64@gmail.com>
2024-01-08 20:51:22 +0300

    task 2-5

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,18 @@
+export default function updateStudentGradeByCity(list, city, newGrade) {
+    return list
+      .filter((obj) => obj.location === city)
+      .map((student) => {
+        newGrade.map((studentGrade) => {
+          if (studentGrade.studentId === student.id) {
+            student.grade = studentGrade.grade;
+          }
+  
+          if (!student.hasOwnProperty('grade')) {
+            student.grade = 'N/A';
+          }
+          return student;
+        });
+  
+        return student;
+      });
+  }
