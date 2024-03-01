ded5dcdb01700a5ac0ab42e07fc79722371a91b2
James Mathenge <mathengejames64@gmail.com>
2024-03-01 03:37:10 +0300

    task 6

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,7 +1,9 @@
-const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
-    if (success) {
-      resolve({data: 'Successful response from the API'});
-    }
-  });
-  
-  module.exports = getPaymentTokenFromAPI;
+function getPaymentTokenFromAPI(success) {
+  if (success) {
+return new Promise((resolve) => {
+        resolve({data: 'Successful response from the API' });
+});
+  }
+}
+
+module.exports = getPaymentTokenFromAPI;
